fb2b15215197a5e4965069627bafd7d9d5c94b81
= <mwkeay@gmail.com>
2025-01-21 14:36:03 +0000

    Added loading CSS

--- src/app/playlists/[id]/Playlist.tsx
+++ src/app/playlists/[id]/Playlist.tsx
@@ -4,20 +4,33 @@ import { FC } from "react";
 import usePlaylist from "./usePlaylist";
 import PlaylistTable from "./PlaylistTable";
 
-const Meta: FC<{ meta: any }> = ({ meta }) => (
-    <div className="flex p-4 gap-4">
-        <div className="w-32 h-32">
-            <img
-                src={ Array.isArray(meta?.images) ? meta.images[0].url : undefined }
-                className="w-full"
-            />
+import "./loading-shimmer.css";
+
+const Meta: FC<{ meta: any }> = ({ meta }) => {
+    if (!meta) return (
+        <div className="flex p-4 gap-4">
+            <div className="w-32 h-32 loading-shimmer" />
+            <div className="flex flex-col loading-shimmer">
+                <h1>Look away! I'm loading!</h1>
+                <p>It is so very rude to try and look at this content while it is still being fetched. Maybe you should fetch some manners.</p>
+            </div>
         </div>
-        <div className="flex flex-col">
-            <h1>{ meta?.name }</h1>
-            <p>{ meta?.description }</p>
+    );
+    return (
+        <div className="flex p-4 gap-4">
+            <div className="w-32 h-32">
+                <img
+                    src={ Array.isArray(meta?.images) ? meta.images[0].url : undefined }
+                    className="w-full"
+                />
+            </div>
+            <div className="flex flex-col">
+                <h1>{ meta?.name }</h1>
+                <p>{ meta?.description }</p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 const Playlist: FC<{ id: string }> = ({ id }) => {
     const { meta, items, isLoading, error } = usePlaylist(id);
