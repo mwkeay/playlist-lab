e3332d7ec2198952a38f98c2b514368de28cc933
= <mwkeay@gmail.com>
2025-03-25 16:47:48 +0000

    feat(PlaylistTable): Add variable columns

--- src/app/playlists/[id]/PlaylistTable.tsx
+++ src/app/playlists/[id]/PlaylistTable.tsx
@@ -1,14 +1,107 @@
 import "./loading-shimmer.css";
-import formatArtistNames from "@/lib/formatArtistNames";
-import { Dispatch, FC, SetStateAction, useState } from "react";
+import { Dispatch, FC, ReactNode, SetStateAction, useState } from "react";
 import formatMilliseconds from "@/lib/formatMilliseconds";
 import { Open_Sans } from "next/font/google";
 import { usePlaylistContext } from "./PlaylistProvider/context";
-import Logger from "@/lib/logger";
 
-const PAGE_LENGTH = 50;
+// Constants
+const PAGE_LENGTH = 100;
+const DEFAULT_COLUMNS: ColumnType[] = ["CUSTOM_ORDER", "NAME", "ARTISTS", "ALBUM", "DURATION"];
 
+// Types
+type ColumnType = "CUSTOM_ORDER" | "NAME" | "ARTISTS" | "ALBUM" | "DURATION";
+
+// Fonts
 const numberFont = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
+
+// Table Mapping
+const columnHeaderMappings: Record<ColumnType, { label: ReactNode; className: string }> = {
+    "CUSTOM_ORDER": {
+        className: "w-12 text-center",
+        label: "#"
+    },
+    "NAME": {
+        className: "flex-1",
+        label: "Title"
+    },
+    "ARTISTS": {
+        className: "flex-1",
+        label: "Artists"
+    },
+    "ALBUM": {
+        className: "flex-1",
+        label: "Album"
+    },
+    "DURATION": {
+        className: "w-20 text-right pr-5",
+        label: <>&#x1F552;</>
+    },
+};
+const columnDataMappings: Record<ColumnType, { className: string, render: (item: any, index?: number) => ReactNode }> = {
+    "CUSTOM_ORDER": {
+        className: "w-12 text-center overflow-hidden",
+        render: (_, index) => index != null ? index + 1 : undefined,
+    },
+    "NAME": {
+        className: "flex-1 overflow-hidden",
+        render: (item) => item?.track?.name ?? undefined,
+    },
+    "ARTISTS": {
+        className: "flex-1 overflow-hidden",
+        render: (item) =>
+            item?.track?.artists?.length
+                ? item.track.artists.map((artist: any) => artist.name).join(", ")
+                : undefined,
+    },
+    "ALBUM": {
+        className: "flex-1 overflow-hidden",
+        render: (item) => item?.track?.album?.name ?? undefined,
+    },
+    "DURATION": {
+        className: `w-20 text-right pr-4 overflow-hidden ${numberFont.className}`,
+        render: (item) => item?.track?.duration_ms ? formatMilliseconds(item.track.duration_ms) : undefined,
+    },
+};
+
+
+/** Table display for a playlists active indexes */
+const PlaylistTable: FC = () => {
+
+    const { items, activeIndexes } = usePlaylistContext();
+    const [page, setPage] = useState(1);
+    const [activeColumns, setActiveColumns] = useState<ColumnType[]>(DEFAULT_COLUMNS);
+
+    return (
+        <>
+            <table className="flex flex-col">
+                <thead>
+                    <tr className="flex">
+                        {activeColumns.map((columnType, i) => {
+                            const { label, className } = columnHeaderMappings[columnType];
+                            return <th key={i+1} className={className}>{label}</th>;
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    { activeIndexes?.slice((page-1)*PAGE_LENGTH, page*PAGE_LENGTH).map((itemIndex, i) => {
+                        const item = items?.[itemIndex];
+                        return (
+                            <tr key={i+1} className="hover:bg-white hover:text-black flex">
+                                {activeColumns.map((columnType, i) => {
+                                    const { className, render } = columnDataMappings[columnType];
+                                    return <td key={i+1} className={className}>
+                                        <p className="whitespace-nowrap truncate">{render(item, itemIndex)}</p>
+                                    </td>
+                                })}
+                            </tr>
+                        );
+                    }) }
+                </tbody>
+            </table>
+            <PageNavigator page={page} setPage={setPage} max={Math.ceil((activeIndexes?.length ?? 1) / PAGE_LENGTH)} />
+        </>
+    );
+};
 
 const PageNavigator: FC<{ page: number, setPage: Dispatch<SetStateAction<number>>, max: number }> = ({ page, setPage, max }) => {
     return (
@@ -25,63 +118,6 @@ const PageNavigator: FC<{ page: number, setPage: Dispatch<SetStateAction<number>
                 className="p-2 w-10 enabled:hover:bg-white enabled:hover:text-black disabled:text-gray-600"
             >&rarr;</button>
         </div>
-    );
-};
-
-const PlaylistTable: FC = () => {
-
-    const { items, activeIndexes } = usePlaylistContext();
-    const [page, setPage] = useState(1);
-
-    return (
-        <>
-            <table className="flex flex-col">
-                <thead>
-                    <tr className="flex">
-                        <th className="w-12">#</th>
-                        <th className="flex-1">Title</th>
-                        <th className="flex-1">Artist</th>
-                        <th className="flex-1">Album</th>
-                        <th className="w-20 text-right pr-5">&#x1F552;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { activeIndexes?.slice((page-1)*PAGE_LENGTH, page*PAGE_LENGTH).map((itemIndex, i) => {
-                        const item = items?.[itemIndex];
-                        // Indexing error
-                        if (!item) {
-                            Logger.error("Playlist index's item not found in records.");
-                            return <tr key={i+1} className="error-bg flex">
-                                <td className="w-12 text-center overflow-hidden">
-                                    <p className="whitespace-nowrap truncate">{ itemIndex + 1 }</p>
-                                </td>
-                            </tr>;
-                        }
-                        // Item found
-                        return (
-                            <tr key={i+1} className="hover:bg-white hover:text-black flex">
-                                <td className="w-12 text-center overflow-hidden">
-                                    <p className="whitespace-nowrap truncate">{ itemIndex + 1 }</p>
-                                </td>
-                                <td className="flex-1 overflow-hidden">
-                                    <p className="whitespace-nowrap truncate">{ item?.track?.name ?? "-" }</p>
-                                </td>
-                                <td className="flex-1 overflow-hidden">
-                                    <p className="whitespace-nowrap truncate">{ item?.track?.artists ? formatArtistNames(item?.track?.artists) : "-" }</p>
-                                </td>
-                                <td className="flex-1 overflow-hidden">
-                                    <p className="whitespace-nowrap truncate">{ item?.track?.album?.name ?? "-" }</p>
-                                </td>
-                                <td className="w-20 text-right pr-4 overflow-hidden">
-                                    <p className={`whitespace-nowrap truncate ${numberFont.className}`}>{ item?.track?.duration_ms ? formatMilliseconds(item.track.duration_ms) : "-" }</p>
-                                </td>
-                            </tr>
-                        );
-                    }) }
-                </tbody>
-            </table>
-            <PageNavigator page={page} setPage={setPage} max={Math.ceil((activeIndexes?.length ?? 1) / PAGE_LENGTH)} />
-        </>
     );
 };
 
