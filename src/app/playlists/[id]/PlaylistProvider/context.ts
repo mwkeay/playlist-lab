6f30c597e47b431cafdb17c021fefc3286352097
= <mwkeay@gmail.com>
2025-04-02 10:15:11 +0100

    chore(PlaylistProvider): added comments

--- src/app/playlists/[id]/PlaylistProvider/context.ts
+++ src/app/playlists/[id]/PlaylistProvider/context.ts
@@ -1,22 +1,27 @@
 import { createContext, useContext } from "react";
 
+// Context object for the Provider
 export interface PlaylistProviderContext {
     activeIndexes?: number[]
     error: Error | null
 }
 
+// Context object for the Consumer
 export interface PlaylistConsumerContext extends PlaylistProviderContext {
     items?: Record<number, any>
     meta: any
 }
 
+// Initial null context object for startup
 export const initContext: PlaylistProviderContext = {
     activeIndexes: undefined,
     error: null,
 };
 
+// Create context
 export const PlaylistContext = createContext<PlaylistConsumerContext | undefined>(undefined);
 
+// Consumer hook to access context
 export const usePlaylistContext = () => {
     const context = useContext(PlaylistContext);
     if (!context) throw Error("Playlist context module must be used within a playlist context provider.");
